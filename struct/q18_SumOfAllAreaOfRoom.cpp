c860c6d83640efd8b79b0db4360068b356e3c99f
priy-am <priyam2101753@gmail.com>
2024-10-22 08:26:16 +0530

    add question 18

--- struct/q18_SumOfAllAreaOfRoom.cpp
+++ struct/q18_SumOfAllAreaOfRoom.cpp
@@ -22,11 +22,33 @@ float AreaOfRoom(struct distance length, struct distance width){
     return area;
 }
 
-void SumOfAreaOfRooms();
+float SumOfAreaOfRooms(float a, float b){
+    return a+b;
+}
 
 
 int main(int argc, char const *argv[])
 {
+    struct Room dinningRoom;
+    struct Room studyRoom;
+    cout<<"Enter the length of dinning room in meter and centimeter:- ";
+    cin>>dinningRoom.length.meter>> dinningRoom.length.centimeter;
+    cout<<"Enter the width of dinning room in meter and centimeter:- ";
+    cin>>dinningRoom.width.meter>> dinningRoom.width.centimeter;
+
+    float a = AreaOfRoom(dinningRoom.length, dinningRoom.width); 
+    cout<<"area of dinningRoom "<<a<<endl;
     
+    cout<<"Enter the length of study room in meter and centimeter:- ";
+    cin>>studyRoom.length.meter>> studyRoom.length.centimeter;
+    cout<<"Enter the width of study room in meter and centimeter:- ";
+    cin>>studyRoom.width.meter>> studyRoom.width.centimeter;
+    float b = AreaOfRoom(studyRoom.length, studyRoom.width);   
+    cout<<"area of studyRoom "<<b<<endl;
+
+    float c = SumOfAreaOfRooms(a, b);
+    int m = c;
+    float cm= c-m;
+    cout<<m<<"meter"<<cm<<"cm"<<endl;
     return 0;
 }
